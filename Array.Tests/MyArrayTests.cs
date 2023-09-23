dd3e499c47138af4a6e8b441203f386bfe13fb14
AKamci <76794253+Scavaliers@users.noreply.github.com>
2023-09-23 22:11:56 +0300

    Latest status

---
+++ Array.Tests/MyArrayTests.cs
@@ -0,0 +1,235 @@
+namespace Array.Tests
+{
+    public class MyArrayTests
+    {
+        [Fact]
+        public void CanAddNumberInArray()
+        {
+            // Arrange
+            var array = new MyArray<int>();
+
+            // Act
+            array.Add(25);
+
+           
+
+            // Assess
+            var result = array.Get(0);
+
+            Assert.Equal(25, result);
+        }
+
+
+        [Fact]
+        public void CanRemoveNumberFromArray()
+        {
+            // Arrange
+            var array = new MyArray<int>();
+
+            // Act
+            array.Add(25);
+            array.RemoveAt(0);
+
+            // Assess
+            var result = array.Get(0);
+
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void CanClearNumberFromArray()
+        {
+            // Arrange
+            var array = new MyArray<int>();
+
+            // Act
+            array.Add(25);
+            array.Add(35);
+            array.Add(55);
+
+            array.Clear();
+
+            // Assess
+            var result = array.Clear().Length;
+
+            Assert.Equal(array.Length, result);
+        }
+
+        [Fact]
+        public void CanContainsGivenNumberInArray()
+        {
+            // Arrange
+            var array = new MyArray<int>();
+
+            // Act
+            array.Add(25);
+
+            // Assess
+            var result = array.Contains(25);
+            
+
+            Assert.Equal(true, result);
+        }
+
+        [Fact]
+        public void CanEnlargeToArray()
+        {
+            //Arrange
+            var array = new MyArray<int>();
+
+            //Act
+            
+           array.Enlarge(175);
+
+            //Assess
+
+            Assert.Equal(175, array.Length);
+
+        }
+
+        [Fact]
+
+        public void CanArrayReset()
+        {
+            //Arange
+            var array = new MyArray<int>();
+
+            //Act
+            array.ArrayReset();
+
+            //Assess
+
+            Assert.Equal(0, array.Length);
+        }
+
+        [Theory]
+        [InlineData(25)]
+        public void CanAddNumberInArrayTheory(int a)
+        {
+            // Arrange
+            var array = new MyArray<int>();
+
+            // Act
+            array.Add(a);
+
+            // Assess
+            var result = array.Get(0);
+
+            Assert.Equal(a, result);
+        }
+        
+        [Theory]
+        [InlineData(25,0)]
+        public void CanRemoveNumberFromArrayTheory(int a,int b)
+        {
+            // Arrange
+            var array = new MyArray<int>();
+
+            // Act
+            array.Add(a);
+            array.RemoveAt(b);
+
+            // Assess
+            var result = array.Get(b);
+
+            Assert.Equal(b, result);
+        }
+
+        [Theory]
+        [InlineData(25,35,45)]
+        public void CanClearNumberFromArrayTheory(int a, int b, int c)
+        {
+            // Arrange
+            var array = new MyArray<int>();
+
+            // Act
+            array.Add(a);
+            array.Add(b);
+            array.Add(c);
+
+            array.Clear();
+
+            // Assess
+            var result = array.Clear().Length;
+
+            Assert.Equal(array.Length, result);
+        }
+
+        [Theory]
+        [InlineData(25)]
+        public void CanContainsGivenNumberInArrayTheory(int a)
+        {
+            // Arrange
+            var array = new MyArray<int>();
+
+            // Act
+            array.Add(a);
+
+            // Assess
+            var result = array.Contains(a);
+
+
+            Assert.Equal(true, result);
+        }
+
+        [Theory]
+        [InlineData(175)]
+        public void CanEnlargeToArrayTheory(int a)
+        {
+            //Arrange
+            var array = new MyArray<int>();
+
+            //Act
+
+            array.Enlarge(a);
+
+            //Assess
+
+            Assert.Equal(a, array.Length);
+
+        }
+
+        [Theory]
+        [InlineData()]
+        public void CanArrayResetTheory()
+        {
+            //Arange
+            var array = new MyArray<int>();
+
+            //Act
+            array.ArrayReset();
+
+            //Assess
+
+            Assert.Equal(0, array.Length);
+        }
+
+
+        [Fact]
+
+        public void ShouldNotEnLargeArray()
+        {
+
+
+            //Arrange
+            var array = new MyArray<int>();
+
+
+            //Act
+
+            
+
+            //Assess
+
+            Assert.Throws<ArrayEnlargeException>(() => array.Enlarge(90)); 
+
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ NUnit_Test/UnitTest1.cs
@@ -0,0 +1,122 @@
+using Array;
+using System;
+
+namespace NUnit_Test
+{
+    public class Tests
+    {
+        private MyArray<int> array;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            array = new MyArray<int>();
+        }
+
+        
+        [Test]
+        public void CanAddNumberInArrayTest()
+        {
+            // Arrange
+            array = new MyArray<int>();
+
+            // Act
+            array.Add(25);
+           
+
+            // Assess
+            var result = array.Get(0);
+
+            Assert.AreEqual(25,result);
+            
+        }
+
+        [Test]
+        public void CanRemoveNumberFromArray()
+        {
+            
+            // Arrange
+            array = new MyArray<int>();
+            int number = 25;
+
+            // Act
+            array.Add(number);
+            array.RemoveAt(0);
+
+            // Assess
+            var result = array.Get(0);
+
+            Assert.AreEqual(result, 0);
+        }
+
+
+
+        [Test]
+        public void CanClearNumberFromArray()
+        {
+            // Arrange
+            array = new MyArray<int>();
+
+            // Act
+            array.Add(25);
+            array.Add(35);
+            array.Add(55);
+
+            array.Clear();
+
+            // Assess
+            var result = array.Clear().Length;
+
+            Assert.AreEqual(array.Length, result);
+        }
+
+        [Test]
+        public void CanContainsGivenNumberInArray()
+        {
+            // Arrange
+            array = new MyArray<int>();
+
+            // Act
+            array.Add(25);
+
+            // Assess
+            var result = array.Contains(25);
+
+
+            Assert.True(result);
+        }
+
+        [Test]
+        public void CanEnlargeToArray()
+        {
+            //Arrange
+            int new_size = 175;
+            array = new MyArray<int>();
+            //Act
+
+            array.Enlarge(new_size);
+
+            //Assess
+
+            Assert.AreEqual(array.Length, 175);
+
+        }
+        [Test]
+        public void CanArrayReset()
+        {
+            //Arange
+            array = new MyArray<int>();
+            //Act
+            array.ArrayReset();
+
+            //Assess
+            
+            Assert.AreEqual(array.Length, 0 );
+        }
+
+
+
+
+    }
+}
