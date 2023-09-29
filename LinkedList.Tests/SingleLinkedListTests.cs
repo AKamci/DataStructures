3d4bb5c0fba899992be894d0312f2e9d42b628f0
Emrah Yurtlu <emrahy@engineerica.com>
2023-09-29 23:10:14 +0300

    Prepared test scenarios for SingleLinkedList.

--- LinkedList.Tests/SingleLinkedListTests.cs
+++ LinkedList.Tests/SingleLinkedListTests.cs
@@ -2,10 +2,28 @@ namespace LinkedList.Tests
 {
     public class SingleLinkedListTests
     {
-        [Fact]
-        public void Test1()
-        {
+        // Write a test for initiating a generic SingleLinkedList instance/object
 
-        }
+        // Write a test for checking the SingleLinkedList is empty
+
+        // Write a test for checking the type of SingleLinkedList
+
+        // Write a test for one adding value
+
+        // Write a test for multiple values
+
+        // Write a test for checking the tail of the SingleLinkedList
+
+        // Write a test for checking the tail of the SingleLinkedList
+
+        // Write a test for removing an element on the list
+
+        // Write a test for addfirst of the SingleLinkedList
+
+        // Write a test for addlast of the SingleLinkedList
+
+        // Write a test for add 5 values to the list and traverse the list and all the numbers are exist
+
+        // Write a test for the list length is exact amount of inserted values
     }
 }
