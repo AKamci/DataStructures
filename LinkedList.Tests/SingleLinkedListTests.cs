808952188bb94b3778054085d52be4a54970e56e
Emrah Yurtlu <emrahy@engineerica.com>
2023-10-03 21:44:09 +0300

    Test metods implemented firstly.

--- LinkedList.Tests/SingleLinkedListTests.cs
+++ LinkedList.Tests/SingleLinkedListTests.cs
@@ -1,121 +1,114 @@
-namespace LinkedList.Tests
+﻿namespace LinkedList.Tests
 {
     public class SingleLinkedListTests
     {
         // Write a test for initiating a generic SingleLinkedList instance/object
+        [Fact]
+        public void ShouldInitiateSingleLinkedList()
+        {
+            // Arrange
+            var linkedList = new SingleLinkedList<int>();
 
-    
+            // Act
+
+            //Assess
+            Assert.NotNull(linkedList);
+        }
+
         // Write a test for checking the SingleLinkedList is empty
-
         [Fact]
         public void ShouldNullSingleLinkedList()
         {
             // Arrange
             SingleLinkedList<int> linkedList = new SingleLinkedList<int>();
+
             // Act
+
             // Assess
-            Assert.Null(linkedList);
+            Assert.Equal(0, linkedList.Count);
         }
 
         // Write a test for checking the type of SingleLinkedList
-
         [Fact]
         public void ShouldTypeSingleLinkedList()
         {
             // Arrange
-
             SingleLinkedList<int> linkedList = new SingleLinkedList<int>();
 
             // Act
+            var type = linkedList.GetType();
 
             // Assess
-
-            Assert.IsType<SingleLinkedList<int>>(linkedList);
-
-
+            Assert.IsType<int>(type);
         }
 
         // Write a test for adding element between two nodes
-
         [Fact]
         public void ShouldAddBetweenTwoNodesSingleLinkedList()
         {
             // Arrange
-
             SingleLinkedList<int> linkedList = new SingleLinkedList<int>();
 
             // Act
-
-            linkedList.Add(35);
-            linkedList.Add(45);
-            linkedList.Add(55);
-            linkedList.Add(65);
-            linkedList.Add(75);
-            linkedList.Add(85);
-            linkedList.Add(95);
-
-            linkedList.AddAfter(5,100);
+            linkedList.AddLast(35);
+            linkedList.AddLast(45);
+            linkedList.AddLast(55);
+            linkedList.AddLast(65);
+            linkedList.AddLast(75);
+            linkedList.AddLast(85);
+            linkedList.AddLast(95);
+            linkedList.AddLast(100);
 
 
             var NumberValue = linkedList.Get(6);
 
             // Assess
-
-            Assert.Equal(100,NumberValue);
+            Assert.Equal(100, NumberValue);
 
         }
-
-        // Write a test for multiple values
-
-        // Write a test for checking the tail of the SingleLinkedList
 
         [Fact]
         public void ShouldCheckTailSingleLinkedList()
         {
             // Arrange
-
             SingleLinkedList<int> linkedList = new SingleLinkedList<int>();
 
             // Act
-
-            linkedList.Add(35);
-            linkedList.Add(45);
-            linkedList.Add(55);
-            linkedList.Add(65);
-            linkedList.Add(75);
-            linkedList.Add(85);
+            linkedList.AddLast(35);
+            linkedList.AddLast(45);
+            linkedList.AddLast(55);
+            linkedList.AddLast(65);
+            linkedList.AddLast(75);
+            linkedList.AddLast(85);
 
 
 
             // Assess
-
             var CheckTail = linkedList.Tail();
-
-
             Assert.Equal(85, CheckTail);
 
         }
 
         // Write a test for removing an element on the list
-
         [Fact]
         public void ShouldRemoveFromSingleLinkedList()
         {
             // Arrange
-
             SingleLinkedList<int> linkedList = new SingleLinkedList<int>();
 
             // Act
+            // AddLast en sona eklemek
+            // AddFirst en başa eklemek
+            // 
 
-            linkedList.Add(35);
-            linkedList.Add(45);
-            linkedList.Add(55);
-            linkedList.Add(65);
-            linkedList.Add(75);
-            linkedList.Add(85);
+            linkedList.AddLast(35);
+            linkedList.AddLast(45);
+            linkedList.AddLast(55);
+            linkedList.AddLast(65);
+            linkedList.AddLast(75);
+            linkedList.AddLast(85);
 
             // Assess
-
             var CheckTail = linkedList.Remove(4);
 
             var Control = linkedList.Contains(65);
@@ -125,69 +118,90 @@ namespace LinkedList.Tests
         }
 
         // Write a test for addfirst of the SingleLinkedList
-
         [Fact]
         public void ShouldAddFirstToSingleLinkedList()
         {
             // Arrange
-
             SingleLinkedList<int> linkedList = new SingleLinkedList<int>();
 
             // Act
-
-            linkedList.Add(35);
-            linkedList.Add(45);
-            linkedList.Add(55);
-            linkedList.Add(65);
-            linkedList.Add(75);
-            linkedList.Add(85);
+            linkedList.AddFirst(35);
+            linkedList.AddFirst(45);
+            linkedList.AddFirst(55);
+            linkedList.AddFirst(65);
+            linkedList.AddFirst(75);
+            linkedList.AddFirst(85);
             linkedList.AddFirst(25);
 
 
             // Assess
-
-            var Check = linkedList.Get(1);
-            Assert.Equal(25, Check);
-
+            var headNode = linkedList.Head();
+            Assert.Equal(25, headNode.Value);
         }
 
-        //????
         // Write a test for addlast of the SingleLinkedList
-
         [Fact]
         public void ShouldAddLastToSingleLinkedList()
         {
             // Arrange
-
             SingleLinkedList<int> linkedList = new SingleLinkedList<int>();
 
             // Act
-
-            linkedList.Add(35);
-            linkedList.Add(45);
-            linkedList.Add(55);
-            linkedList.Add(65);
-            linkedList.Add(75);
-            linkedList.Add(85);
+            linkedList.AddLast(35);
+            linkedList.AddLast(45);
+            linkedList.AddLast(55);
+            linkedList.AddLast(65);
+            linkedList.AddLast(75);
+            linkedList.AddLast(85);
             linkedList.AddLast(25);
 
-            
+
             // Assess
-
-            var Check = linkedList.Get(7);
-            Assert.Equal(25, Check);
-
+            var tailNode = linkedList.Tail();
+            Assert.Equal(25, tailNode.Value);
         }
 
-        // Write a test for add 5 values to the list and traverse the list and all the numbers are exist
+        // Write a test for add 5 values to the list and loop on the list and all the numbers are exist
+        [Fact]
+        public void ShouldAddFiveNumbersAndLoopOnList()
+        {
+            // Arrange
+            SingleLinkedList<int> linkedList = new SingleLinkedList<int>();
+
+            // Act
+            var listOfIntegers = new List<int> { 35, 45, 55, 65, 75 };
+            listOfIntegers.ForEach(t => linkedList.AddLast(t));
+
+            var testResult = false;
+            var node = linkedList.Head();
+            while (node.Next != null)
+            {
+                if (!listOfIntegers.Contains(node.Value))
+                {
+                    testResult = false;
+                    break;
+                }
+                node = node.Next;
+                testResult = true;
+            }
+
+            // Assert
+            Assert.True(testResult);
+        }
+
         // Write a test for the list length is exact amount of inserted values
+        [Fact]
+        public void ShouldAddFiveNumbersAndCheckTheCOuntOfListAs5()
+        {
+            // Arrange
+            SingleLinkedList<int> linkedList = new SingleLinkedList<int>();
 
+            // Act
+            var listOfIntegers = new List<int> { 35, 45, 55, 65, 75 };
+            listOfIntegers.ForEach(t => linkedList.AddLast(t));
 
-
-
-
-
-
-
+            // Assert
+            Assert.Equal(5, linkedList.Count);
+        }
     }
 }
